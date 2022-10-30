ff9ecf9683110e9880e8e4061930b58b614784d4
Bladyzamosc <Bladyzamosc@gmail.com>
2022-10-30 10:55:23 +0100

    Empty quarkus application

---
+++ src/main/java/com/bladyzamosc/quarkusek/QuarkusekApp.java
@@ -0,0 +1,17 @@
+package com.bladyzamosc.quarkusek;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+/**
+ * User: Z6EKI
+ * Date: 30.10.2022
+ */
+@QuarkusMain
+public class QuarkusekApp
+{
+  public static void main(String[] args)
+  {
+    Quarkus.run(args);
+  }
+}
